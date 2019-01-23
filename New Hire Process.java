d98598bb1eeaaa1045dc07d8147def0dff4e8a78
Jael Flaquer <jflaquer@udel.edu>
2019-01-23 12:11:01 -0500

    Uploading the initial "very rough draft" template for New Hire Automation program

---
+++ New Hire Process.java
@@ -0,0 +1,59 @@
+/* Program to automate the New Hire Process */
+//Imported Java utilities
+
+
+
+//Class for New Hire Process Program
+
+
+//Global Variables (Privatize everything if possible)
+private String adminUsername;
+private String adminPassword;
+private String newHireFirstName;
+private Char newHireMiddleInitial;
+private String newHireLastName;
+private String newHireEmail;
+private String newHireUsername;
+private int newHireExtension;
+private String newHirePassword = "Career123!";
+
+//Function to create an object that will store the adminInfo
+
+private String adminInfo(String request){
+
+}
+
+//Function to create an object that will store the UserInfo
+
+private String userInfo(String request){
+    
+}
+
+//Function to ask for -A credentials
+
+private String adminCreds(String request){
+    
+}
+
+//Function to ask for New Hire credentials
+
+private String newHireCredRequest(){
+
+}
+
+//Function to navigate to Active Directory
+
+private void activeDirectoryNav(){
+    
+}
+
+//Function to open Exchange Portal and Navigate through the 
+
+
+
+//
+
+//Main function to run everything in the correct order
+public static void main(String args[]) {
+    
+}
