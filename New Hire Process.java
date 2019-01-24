03a353dddadc7d6f10e9799c12f1ce7ef001f88d
Jael Flaquer <jflaquer@udel.edu>
2019-01-24 10:13:18 -0500

    Updated a few mistakes and added a new blank function for Office 365 Navigation

--- New Hire Process.java
+++ New Hire Process.java
@@ -1,58 +1,65 @@
 /* Program to automate the New Hire Process */
-//Imported Java utilities
+//Imported Java utilities (I will probably be using Selenium to perform actions on webpages)
 
 
 
 //Class for New Hire Process Program
+private class NewHirePocess {
 
+    //Global Variables (Privatize everything if possible)
+    private String adminUsername;
+    private String adminPassword;
+    private String newHireFirstName;
+    private Char newHireMiddleInitial;
+    private String newHireLastName;
+    private String newHireEmail;
+    private String newHireUsername;
+    private int newHireExtension;
+    private String newHirePassword = "Career123!";
 
-//Global Variables (Privatize everything if possible)
-private String adminUsername;
-private String adminPassword;
-private String newHireFirstName;
-private Char newHireMiddleInitial;
-private String newHireLastName;
-private String newHireEmail;
-private String newHireUsername;
-private int newHireExtension;
-private String newHirePassword = "Career123!";
+    //Function to create an object that will store the adminInfo
 
-//Function to create an object that will store the adminInfo
+    private String adminInfo(String request){
 
-private String adminInfo(String request){
+    }
 
-}
+    //Function to create an object that will store the UserInfo
 
-//Function to create an object that will store the UserInfo
+    private String userInfo(String request){
+        
+    }
 
-private String userInfo(String request){
-    
-}
+    //Function to ask for -A credentials
 
-//Function to ask for -A credentials
+    private String adminCreds(String request){
+        
+    }
 
-private String adminCreds(String request){
-    
-}
+    //Function to ask for New Hire's credentials
 
-//Function to ask for New Hire credentials
+    private String newHireCredRequest(){
 
-private String newHireCredRequest(){
+    }
 
-}
+    //Function to navigate to Active Directory
 
-//Function to navigate to Active Directory
+    private void activeDirectoryNav(){
+        
+    }
 
-private void activeDirectoryNav(){
-    
-}
+    //Function to open Exchange Portal and Navigate through the 
+    private void exchangePortalNav(){
 
-//Function to open Exchange Portal and Navigate through the 
-private void exchangePortalNav(){
+    }
 
-}
+    //Function to navigate the Office 365 setup
+    private void office365Nav(){
+        
+    }
+
+}//End Class
 
 //Main function to run everything in the correct order
-public static void main(String args[]) {
-    
+public static void main(String[] args) {
+    print("Hello World");
 }
